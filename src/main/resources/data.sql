a825c1da5d8c02ed130ced7c60b5d05417393d2b
T.Sanyi <s.truczkai@gmail.com>
2023-03-21 09:48:27 +0100

    add data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into doctor (id, name)
+values  (1, 'Dr. Bubó'),
+        (2, 'Dr. Furcsa');
+
+insert into patient (id, name, date_of_birth, doctor_id)
+values  (1, 'Gipsz Jakab', '1985-06-21', 1),
+        (2, 'John Doe', '1999-02-21', 1);
