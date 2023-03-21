8e1178900e214045622d7542adeb15ce70118142
T.Sanyi <s.truczkai@gmail.com>
2023-03-21 10:43:17 +0100

    change DoctorController to Controller from RestController

--- src/main/java/com/codecool/jpa/medicinejpa/controller/DoctorController.java
+++ src/main/java/com/codecool/jpa/medicinejpa/controller/DoctorController.java
@@ -2,21 +2,30 @@ package com.codecool.jpa.medicinejpa.controller;
 
 import com.codecool.jpa.medicinejpa.model.Doctor;
 import com.codecool.jpa.medicinejpa.repository.DoctorDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class DoctorController {
     private final DoctorDao doctorDao;
 
     public DoctorController(DoctorDao doctorDao) {
         this.doctorDao = doctorDao;
     }
-
+    @ResponseBody
     @GetMapping("/doctors")
-    public List<Doctor> findAll(){
+    public List<Doctor> findAll() {
         return doctorDao.findAll();
+    }
+
+    @GetMapping("/doctor")
+    public String index(Model model) {
+        model.addAttribute("doctors", doctorDao.findAll());
+        return "index";
     }
 }
